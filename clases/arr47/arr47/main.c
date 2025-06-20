58e94292d9170b9caa81150e7fe146c8ebaa5f6e
pc servidor 2 <yonieleusebio@gmail.com>
2025-06-20 10:05:51 -0400

    si

---
+++ clases/arr47/arr47/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int almario [5];
+    almario [0] = 100;
+    almario [0] = 101;
+    almario [0] = 102;
+    almario [0] = 103;
+    almario [0] = 104;
+    for(int i = 0;i<5;i++)
+    {
+        printf("\nValor de amario1")
+    }
+    return 0;
+}
